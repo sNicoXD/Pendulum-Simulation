aba9d4ccebc788fa363d0ce5ffa26ebc4dcf7b4e
Nico <Nico61720@gmail.com>
2022-11-21 11:16:06 -0500

    Initialized PyGame and display screen color modify was add

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import pygame,sys
+import pygame,sys,color
+pygame.init()
+
+
+size = width, height = 800, 800
+screen = pygame.display.set_mode(size)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+    screen.fill(color.RED)
+    pygame.display.flip()
+    
